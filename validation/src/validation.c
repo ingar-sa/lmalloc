9c5adba0592a247d18bb9e6f6103f582956415c2
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2025-03-27 16:55:16 +0100

    Tested timing functionality
    Fun fact: from exactly 2936 bytes to 2937, malloc goes from being 1.15
    +- 0.1x slower than the arena to 3.7-4.3x slower. And from exactly
    128424 bytes to 128425, it goes from that to 6.8-12.8x slower. Simply
    fascinating!

--- validation/src/validation.c
+++ validation/src/validation.c
@@ -18,13 +18,7 @@ void u_arena_test(void)
 	long long start = lm_get_time_stamp(PROC_CPUTIME);
 	u_arena *a = u_arena_create(4096, false, 16);
 	long long end = lm_get_time_stamp(PROC_CPUTIME);
-	lm_log_timing(start, end, "arena creation", NS, DBG,
-		      LM_LOG_MODULE_LOCAL);
 	lm_log_timing(start, end, "arena creation", US, DBG,
-		      LM_LOG_MODULE_LOCAL);
-	lm_log_timing(start, end, "arena creation", MS, DBG,
-		      LM_LOG_MODULE_LOCAL);
-	lm_log_timing(start, end, "arena creation", S, DBG,
 		      LM_LOG_MODULE_LOCAL);
 
 	ptrdiff_t a_to_a_mem_diff = LmPtrDiff(a->mem, a);
@@ -33,7 +27,7 @@ void u_arena_test(void)
 	start = lm_get_time_stamp(PROC_CPUTIME);
 	int *array = u_arena_array(a, int, 10);
 	end = lm_get_time_stamp(PROC_CPUTIME);
-	lm_log_timing(start, end, "int array allocation: ", NS, DBG,
+	lm_log_timing(start, end, "int array allocation: ", US, DBG,
 		      LM_LOG_MODULE_LOCAL);
 
 	ptrdiff_t array_size = LmPtrDiff(u_arena_pos(a), array);
@@ -51,16 +45,38 @@ void u_arena_contiguous_test(void)
 	LmLogDebug("Arena with contiguous memory test");
 
 	size_t l1_cache_line_size = lm_get_l1_cache_line_size();
+
 	long long start = lm_get_time_stamp(PROC_CPUTIME);
-	u_arena *a = u_arena_create(4096 - l1_cache_line_size, true, 16);
+	u_arena *a =
+		u_arena_create(LmMebiByte(1) - l1_cache_line_size, true, 16);
 	long long end = lm_get_time_stamp(PROC_CPUTIME);
-	lm_log_timing(start, end, "arena creation", NS, DBG,
+	lm_log_timing(start, end, "arena creation", US, DBG,
 		      LM_LOG_MODULE_LOCAL);
 
 	ptrdiff_t a_to_a_mem_diff = LmPtrDiff(a->mem, a);
 	LmLogDebug("Distance from arena to its memory: %zd", a_to_a_mem_diff);
 
-	int *array = u_arena_array(a, int, 10);
+//#define N_BYTES (734 * sizeof(int) + 1)
+#define N_BYTES (LmKibiByte(125) + 425)
+
+	start = lm_get_time_stamp(PROC_CPUTIME);
+	uint8_t *array = u_arena_alloc_no_align(a, N_BYTES);
+	end = lm_get_time_stamp(PROC_CPUTIME);
+	lm_log_timing(start, end, "int array arena", US, DBG,
+		      LM_LOG_MODULE_LOCAL);
+	long long arena_time = end - start;
+
+	start = lm_get_time_stamp(PROC_CPUTIME);
+	uint8_t *array2 = malloc(N_BYTES);
+	end = lm_get_time_stamp(PROC_CPUTIME);
+	lm_log_timing(start, end, "int array malloc", US, DBG,
+		      LM_LOG_MODULE_LOCAL);
+	long long malloc_time = end - start;
+
+	struct timing_comp tc;
+	int res = lm_compare_timing(malloc_time, arena_time, &tc);
+	lm_log_timing_comp(tc, res, DBG, LM_LOG_MODULE_LOCAL);
+
 	ptrdiff_t array_size = LmPtrDiff(u_arena_pos(a), array);
 	bool array_is_aligned = LmIsAligned(array_size, a->alignment);
 
@@ -73,7 +89,7 @@ void u_arena_contiguous_test(void)
 
 int main(void)
 {
-	u_arena_test();
+	//u_arena_test();
 	u_arena_contiguous_test();
 	return 0;
 }
