c81c7d843861731a08076d1cf668e22acee57724
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2025-04-15 10:34:35 +0200

    Removed unused functionality from system_info

--- src/utils/system_info.c
+++ src/utils/system_info.c
@@ -11,77 +11,22 @@ LM_LOG_REGISTER(system_info);
 // NOTE: Original written by claude
 size_t get_page_size(void)
 {
-	ssize_t page_size = sysconf(_SC_PAGESIZE);
-
+	long page_size = sysconf(_SC_PAGESIZE);
 	if (page_size == -1) {
 		if (errno == 0)
-			LmLogWarning("Page size is indeterminate");
+			LmAssert(0, "Page size is indeterminate");
 		else
-			LmLogError("Failed to get page size (%s)",
-				   strerror(errno));
-
-		exit(EXIT_FAILURE);
+			LmAssert(0, "Failed to get page size (%s)",
+				 strerror(errno));
 	}
 
 	return (size_t)page_size;
 }
 
-// NOTE: Original written by claude
-struct cache_info get_cpu_cache_info(void)
-{
-	struct cache_info info = { 0 };
-
-	// L1 Data Cache
-	info.l1d_cache_size = sysconf(_SC_LEVEL1_DCACHE_SIZE);
-	info.l1d_cache_line = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
-	info.l1d_cache_assoc = sysconf(_SC_LEVEL1_DCACHE_ASSOC);
-
-	// L1 Instruction Cache
-	info.l1i_cache_size = sysconf(_SC_LEVEL1_ICACHE_SIZE);
-	info.l1i_cache_line = sysconf(_SC_LEVEL1_ICACHE_LINESIZE);
-	info.l1i_cache_assoc = sysconf(_SC_LEVEL1_ICACHE_ASSOC);
-
-	// L2 Cache
-	info.l2_cache_size = sysconf(_SC_LEVEL2_CACHE_SIZE);
-	info.l2_cache_line = sysconf(_SC_LEVEL2_CACHE_LINESIZE);
-	info.l2_cache_assoc = sysconf(_SC_LEVEL2_CACHE_ASSOC);
-
-	// L3 Cache
-	info.l3_cache_size = sysconf(_SC_LEVEL3_CACHE_SIZE);
-	info.l3_cache_line = sysconf(_SC_LEVEL3_CACHE_LINESIZE);
-	info.l3_cache_assoc = sysconf(_SC_LEVEL3_CACHE_ASSOC);
-
-	return info;
-}
-
 size_t get_l1d_cacheln_sz(void)
 {
-	size_t l1d_cache_line = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
-	return l1d_cache_line;
-}
-
-// NOTE: Original written by claude
-void print_cache_info(struct cache_info info)
-{
-	printf("CPU Cache Information:\n");
-
-	printf("\nL1 Data Cache:\n");
-	printf("  Size: %ld bytes\n", info.l1d_cache_size);
-	printf("  Line size: %ld bytes\n", info.l1d_cache_line);
-	printf("  Associativity: %ld\n", info.l1d_cache_assoc);
-
-	printf("\nL1 Instruction Cache:\n");
-	printf("  Size: %ld bytes\n", info.l1i_cache_size);
-	printf("  Line size: %ld bytes\n", info.l1i_cache_line);
-	printf("  Associativity: %ld\n", info.l1i_cache_assoc);
-
-	printf("\nL2 Cache:\n");
-	printf("  Size: %ld bytes\n", info.l2_cache_size);
-	printf("  Line size: %ld bytes\n", info.l2_cache_line);
-	printf("  Associativity: %ld\n", info.l2_cache_assoc);
-
-	printf("\nL3 Cache:\n");
-	printf("  Size: %ld bytes\n", info.l3_cache_size);
-	printf("  Line size: %ld bytes\n", info.l3_cache_line);
-	printf("  Associativity: %ld\n", info.l3_cache_assoc);
+	long l1d_cache_line = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+	LmAssert(l1d_cache_line > 0,
+		 "L1 cache line size returned from sysconf was <= 0");
+	return (size_t)l1d_cache_line;
 }
--- src/utils/system_info.h
+++ src/utils/system_info.h
@@ -3,24 +3,6 @@
 
 #include <unistd.h>
 
-struct cache_info {
-	ssize_t l1d_cache_size; // L1 data cache size in bytes
-	ssize_t l1d_cache_line; // L1 data cache line size in bytes
-	ssize_t l1d_cache_assoc; // L1 data cache associativity
-
-	ssize_t l1i_cache_size; // L1 instruction cache size in bytes
-	ssize_t l1i_cache_line; // L1 instruction cache line size in bytes
-	ssize_t l1i_cache_assoc; // L1 instruction cache associativity
-
-	ssize_t l2_cache_size; // L2 cache size in bytes
-	ssize_t l2_cache_line; // L2 cache line size in bytes
-	ssize_t l2_cache_assoc; // L2 cache associativity
-
-	ssize_t l3_cache_size; // L3 cache size in bytes
-	ssize_t l3_cache_line; // L3 cache line size in bytes
-	ssize_t l3_cache_assoc; // L3 cache associativity
-};
-
 size_t get_page_size(void);
 size_t get_l1d_cacheln_sz(void);
 
