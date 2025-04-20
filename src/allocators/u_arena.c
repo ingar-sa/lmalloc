da7fe855a94fc332babd35339c75e5846f357a28
Ingar Solveigson Asheim <ingara@stud.ntnu.no>
2025-04-20 11:12:00 +0200

    Made boolean fields in arena into flag bitmap and added thread arenas

--- src/allocators/u_arena.c
+++ src/allocators/u_arena.c
@@ -21,8 +21,8 @@ static size_t arena_cache_aligned_sz(void)
 	return arena_cache_aligned_sz;
 }
 
-void u_arena_init(UArena *a, bool contiguous, bool mallocd, size_t alignment,
-		  size_t page_sz, size_t cap, uint8_t *mem)
+void ua_init(UArena *ua, bool contiguous, bool mallocd, bool bootstrapped,
+	     size_t alignment, size_t page_sz, size_t cap, uint8_t *mem)
 {
 	LmAssert(
 		LmIsPowerOfTwo(alignment) && 8 <= alignment &&
@@ -30,19 +30,23 @@ void u_arena_init(UArena *a, bool contiguous, bool mallocd, size_t alignment,
 		"Provided alignment is not a power of two and/or aligment not in [8, %zd]",
 		page_sz);
 
-	a->contiguous = contiguous;
-	a->mallocd = mallocd;
-	a->alignment = alignment;
-	a->page_sz = page_sz;
-	a->cur = 0;
-	a->cap = cap;
-	a->mem = mem;
+	ua->flags = 0;
+	if (contiguous)
+		UaSetIsContiguous(ua->flags);
+	if (mallocd)
+		UaSetIsMallocd(ua->flags);
+	if (bootstrapped)
+		UaSetIsBootstrapped(ua->flags);
+	ua->alignment = alignment;
+	ua->page_sz = page_sz;
+	ua->cur = 0;
+	ua->cap = cap;
+	ua->mem = mem;
 }
 
-UArena *u_arena_create(size_t cap, bool contiguous, bool mallocd,
-		       size_t alignment)
+UArena *ua_create(size_t cap, bool contiguous, bool mallocd, size_t alignment)
 {
-	UArena *arena;
+	UArena *ua;
 	uint8_t *mem;
 	size_t page_sz = get_page_size();
 	size_t cacheln_sz = get_l1d_cacheln_sz();
@@ -52,69 +56,95 @@ UArena *u_arena_create(size_t cap, bool contiguous, bool mallocd,
 	if (mallocd) {
 		if (contiguous) {
 			size_t allocation_sz = arena_cache_aligned_sz + cap;
-			arena = malloc(allocation_sz);
-			mem = (uint8_t *)arena + arena_cache_aligned_sz;
+			ua = malloc(allocation_sz);
+			mem = (uint8_t *)ua + arena_cache_aligned_sz;
 		} else {
-			arena = malloc(sizeof(UArena));
+			ua = malloc(sizeof(UArena));
 			mem = malloc(cap);
 		}
 	} else {
 		if (cap % page_sz != 0) {
+			size_t padding = LmPaddingToAlign(cap, page_sz);
 			LmLogWarning(
-				"Creating an mmap'd arena of a size that is not a multiple of page size is disallowed");
-			return NULL;
+				"Creating an mmap'd arena of a size that is "
+				"not a multiple of page size is disallowed. "
+				"Padding %zd bytes with %zd to align to page size",
+				cap, padding);
+			cap += padding;
+			//return NULL;
 		}
 		if (contiguous) {
 			size_t allocation_sz = arena_cache_aligned_sz + cap;
 			mem = mmap(NULL, allocation_sz, PROT_READ | PROT_WRITE,
 				   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-			arena = (UArena *)((uintptr_t)mem);
+			ua = (UArena *)((uintptr_t)mem);
 			mem += arena_cache_aligned_sz;
 		} else {
-			arena = malloc(sizeof(UArena));
+			ua = malloc(sizeof(UArena));
 			mem = mmap(NULL, cap, PROT_READ | PROT_WRITE,
 				   MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 		}
 	}
 
-	u_arena_init(arena, contiguous, mallocd, alignment, page_sz, cap, mem);
-	return arena;
+	ua_init(ua, contiguous, mallocd, false, alignment, page_sz, cap, mem);
+	return ua;
 }
 
-void u_arena_destroy(UArena **ap)
+void ua_destroy(UArena **uap)
 {
-	if (ap && *ap) {
-		UArena *a = *ap;
-		if (!a->mem) {
+	if (uap && *uap) {
+		UArena *ua = *uap;
+		if (UaIsBootstrapped(ua->flags))
+			return;
+		if (!ua->mem) {
 			LmLogWarning("Arena memory was NULL");
 			return;
 		}
-		if (a->mallocd) {
-			if (a->contiguous) {
-				free(a);
+		if (UaIsMallocd(ua->flags)) {
+			if (UaIsContiguous(ua->flags)) {
+				free(ua);
 			} else {
-				free(a->mem);
-				free(a);
+				free(ua->mem);
+				free(ua);
 			}
 		} else {
-			if (a->contiguous) {
-				munmap(a, arena_cache_aligned_sz() + a->cap);
+			if (UaIsContiguous(ua->flags)) {
+				munmap(ua, arena_cache_aligned_sz() + ua->cap);
 			} else {
-				munmap(a->mem, a->cap);
-				free(a);
+				munmap(ua->mem, ua->cap);
+				free(ua);
 			}
 		}
-		*ap = NULL;
+		*uap = NULL;
 	}
 }
 
-inline void *u_arena_alloc(UArena *a, size_t size)
+UArena *ua_bootstrap(UArena *ua, UArena *new_existing, size_t cap,
+		     size_t alignment)
+{
+	UArena *new;
+	if (new_existing)
+		new = new_existing;
+	else
+		new = UaPushStruct(ua, UArena);
+
+	uint8_t *mem = ua_zalloc(ua, cap);
+	if (!new || !mem) {
+		LmLogWarning("Insufficient memory to bootstrap arena");
+		return NULL;
+	}
+
+	ua_init(new, false, false, true, alignment, ua->page_sz, cap, mem);
+	return new;
+}
+
+inline void *ua_alloc(UArena *ua, size_t size)
 {
 	void *ptr = NULL;
-	size_t aligned_sz = size + LmPaddingToAlign(size, a->alignment);
-	if (LM_LIKELY(a->cur + aligned_sz <= a->cap)) {
-		ptr = a->mem + a->cur;
-		a->cur += aligned_sz;
+	size_t aligned_sz = size + LmPaddingToAlign(size, ua->alignment);
+	if (LM_LIKELY(ua->cur + aligned_sz <= ua->cap)) {
+		ptr = ua->mem + ua->cur;
+		ua->cur += aligned_sz;
 	}
 
 	return ptr;
@@ -123,82 +153,149 @@ inline void *u_arena_alloc(UArena *a, size_t size)
 // NOTE: (isa): See 'poc/page_zalloc/u_arena.c' for a short
 // discussion on why zeroing individual allocations is better
 // than pre-zeroing larger chunks
-inline void *u_arena_zalloc(UArena *a, size_t size)
+inline void *ua_zalloc(UArena *ua, size_t size)
 {
 	void *ptr = NULL;
-	size_t aligned_sz = size + LmPaddingToAlign(size, a->alignment);
-	if (LM_LIKELY(a->cur + aligned_sz <= a->cap)) {
-		ptr = a->mem + a->cur;
-		a->cur += aligned_sz;
+	size_t aligned_sz = size + LmPaddingToAlign(size, ua->alignment);
+	if (LM_LIKELY(ua->cur + aligned_sz <= ua->cap)) {
+		ptr = ua->mem + ua->cur;
+		ua->cur += aligned_sz;
 		explicit_bzero(ptr, aligned_sz);
 	}
 	return ptr;
 }
 
-inline void *u_arena_falloc(UArena *a, size_t size)
+inline void *ua_falloc(UArena *ua, size_t size)
 {
-	size_t aligned_sz = size + LmPaddingToAlign(size, a->alignment);
-	void *ptr = a->mem + a->cur;
-	a->cur += aligned_sz;
+	size_t aligned_sz = size + LmPaddingToAlign(size, ua->alignment);
+	void *ptr = ua->mem + ua->cur;
+	ua->cur += aligned_sz;
 	return ptr;
 }
 
-inline void *u_arena_fzalloc(UArena *a, size_t size)
+inline void *ua_fzalloc(UArena *ua, size_t size)
 {
-	size_t aligned_sz = size + LmPaddingToAlign(size, a->alignment);
-	void *ptr = a->mem + a->cur;
-	a->cur += aligned_sz;
+	size_t aligned_sz = size + LmPaddingToAlign(size, ua->alignment);
+	void *ptr = ua->mem + ua->cur;
+	ua->cur += aligned_sz;
 	explicit_bzero(ptr, aligned_sz);
 	return ptr;
 }
 
-void u_arena_release(UArena *a, size_t pos, size_t size)
+void ua_release(UArena *ua, size_t pos, size_t size)
 {
-	LmAssert(pos < a->cur, "pos > arena pos");
-	LmAssert(pos % a->page_sz == 0, "pos is not a multiple of page size");
-	LmAssert(size % a->page_sz == 0, "size is not a multiple of page size");
-	if (LM_UNLIKELY(a->mallocd)) {
+	LmAssert(pos < ua->cur, "pos > arena pos");
+	LmAssert(pos % ua->page_sz == 0, "pos is not a multiple of page size");
+	LmAssert(size % ua->page_sz == 0,
+		 "size is not a multiple of page size");
+	if (LM_UNLIKELY(UaIsMallocd(ua->flags))) {
 		LmLogWarning("Cannot call u_arena_release on mallocd arena");
 	} else {
-		if (madvise(a->mem + pos, size, MADV_DONTNEED) != 0)
+		if (madvise(ua->mem + pos, size, MADV_DONTNEED) != 0)
 			LmLogError("madvise failed: %s", strerror(errno));
 	}
 }
 
-inline void u_arena_free(UArena *a)
+inline void ua_free(UArena *ua)
 {
-	a->cur = 0;
+	ua->cur = 0;
 }
 
-inline void u_arena_pop(UArena *a, size_t size)
+inline void ua_pop(UArena *ua, size_t size)
 {
-	if (LM_LIKELY(((size % a->alignment) == 0) &&
-		      ((ssize_t)a->cur - (ssize_t)size >= 0))) {
-		a->cur -= size;
+	if (LM_LIKELY(((size % ua->alignment) == 0) &&
+		      ((ssize_t)ua->cur - (ssize_t)size >= 0))) {
+		ua->cur -= size;
 	}
 }
 
-void u_arena_set_alignment(UArena *a, size_t alignment)
+void ua_set_alignment(UArena *ua, size_t alignment)
 {
-	if (LM_LIKELY(LmIsPowerOfTwo(alignment) && alignment <= a->page_sz))
-		a->alignment = alignment;
+	if (LM_LIKELY(LmIsPowerOfTwo(alignment) && alignment <= ua->page_sz))
+		ua->alignment = alignment;
 	else
 		LmLogWarning(
 			"Specified alignment is not a power of two or is > page size");
 }
 
-inline void *u_arena_pos(UArena *a)
+inline size_t ua_pos(UArena *ua)
 {
-	void *pos = a->mem + a->cur;
+	size_t pos = ua->cur;
 	return pos;
 }
 
-inline void *u_arena_seek(UArena *a, size_t pos)
+inline void *ua_seek(UArena *ua, size_t pos)
 {
-	if (LM_LIKELY(pos <= a->cap && (pos % a->alignment) == 0)) {
-		a->cur = pos;
-		return a->mem + a->cur;
+	if (LM_LIKELY(pos <= ua->cap && (pos % ua->alignment) == 0)) {
+		ua->cur = pos;
+		return ua->mem + ua->cur;
 	}
 
 	return NULL;
+}
+
+inline size_t ua_reserve(UArena *ua, size_t sz)
+{
+	if (LM_LIKELY(ua->cur + sz <= ua->cap)) {
+		ua->cur += sz;
+		return sz;
+	} else {
+		return ua->cap - ua->cur;
+	}
+}
+
+void ua__thread_arenas_init__(UArena *ta_buf[], struct ua__thread_arenas__ *tas,
+			      struct ua__thread_arenas__ **ta_instance)
+{
+	tas->uas = ta_buf;
+	*ta_instance = tas;
+}
+
+void ua__thread_arenas_init_extern__(struct ua__thread_arenas__ *tas,
+				     struct ua__thread_arenas__ **ta_instance)
+{
+	*ta_instance = tas;
+}
+
+int ua__thread_arenas_add__(UArena *a, struct ua__thread_arenas__ *ta_instance)
+{
+	if (ta_instance->count + 1 <= ta_instance->max_count) {
+		ta_instance->uas[ta_instance->count++] = a;
+		return 0;
+	}
+
+	return 1;
+}
+
+UArenaScratch ua_scratch_begin(UArena *ua)
+{
+	UArenaScratch uas;
+	uas.ua = ua;
+	uas.f5 = ua->cur;
+	return uas;
+}
+
+UArenaScratch ua__scratch_get__(UArena **conflicts, int conflict_count,
+				struct ua__thread_arenas__ *tas)
+{
+	UArena *ua = NULL;
+	if (conflict_count > 0) {
+		for (int i = 0; i < tas->count; ++i) {
+			for (int j = 0; j < conflict_count; ++j) {
+				if (tas->uas[i] != conflicts[j]) {
+					ua = tas->uas[i];
+					goto exit;
+				}
+			}
+		}
+	} else {
+		ua = tas->uas[0];
+	}
+exit:
+	return (ua == NULL) ? (UArenaScratch){ 0 } : ua_scratch_begin(ua);
+}
+
+void ua__scratch_release__(UArenaScratch uas)
+{
+	ua_seek(uas.ua, uas.f5);
 }
--- src/allocators/u_arena.h
+++ src/allocators/u_arena.h
@@ -15,9 +15,20 @@
 #define U_ARENA_MALLOCD true
 #define U_ARENA_NOT_MALLOCD false
 
-typedef struct u_arena {
-	bool contiguous;
-	bool mallocd;
+#define UA_CONTIGUOUS_BIT 0
+#define UA_MALLOCD_BIT 1
+#define UA_BOOTSTRAPPED_BIT 2
+
+#define UaIsContiguous(flags) (!!((flags >> 0) & 1))
+#define UaIsMallocd(flags) (!!((flags >> 1) & 1))
+#define UaIsBootstrapped(flags) (!!((flags >> 2) & 1))
+
+#define UaSetIsContiguous(flags) flags |= 1
+#define UaSetIsMallocd(flags) flags |= (1 << 1)
+#define UaSetIsBootstrapped(flags) flags |= (1 << 2)
+
+typedef struct {
+	uint_least64_t flags;
 	size_t alignment;
 	size_t page_sz;
 	size_t cap;
@@ -25,36 +36,109 @@ typedef struct u_arena {
 	uint8_t *mem;
 } UArena;
 
-void u_arena_init(UArena *a, bool contiguous, bool mallocd, size_t alignment,
-		  size_t page_sz, size_t cap, uint8_t *mem);
+typedef struct {
+	UArena *ua;
+	size_t f5;
+} UArenaScratch;
 
-UArena *u_arena_create(size_t cap, bool contiguous, bool mallocd,
-		       size_t alignment);
+struct ua__thread_arenas__ {
+	UArena **uas;
+	int count;
+	int max_count;
+};
 
-void u_arena_destroy(UArena **ap);
+#define UA_THREAD_ARENAS_REGISTER(thread_name, count_)                      \
+	static __thread UArena *LM_CONCAT3(ua__thread_arenas_, thread_name, \
+					   _buffer__)[count_]               \
+		__attribute__((used));                                      \
+	__thread struct ua__thread_arenas__ LM_CONCAT3(ua__thread_arenas_,  \
+						       thread_name, __)     \
+		__attribute__((used)) = { .uas = NULL,                      \
+					  .count = 0,                       \
+					  .max_count = count_ };            \
+	static __thread struct ua__thread_arenas__                          \
+		*ua__thread_arenas_instance__ __attribute__((used))
 
-void *u_arena_alloc(UArena *a, size_t size);
+#define UA_THREAD_ARENAS_EXTERN(thread_name)                   \
+	extern __thread struct ua__thread_arenas__ LM_CONCAT3( \
+		ua__thread_arenas_, thread_name, __);          \
+	static __thread struct ua__thread_arenas__             \
+		*ua__thread_arenas_instance__ __attribute__((used))
 
-void *u_arena_zalloc(UArena *a, size_t size);
+// WARN: Must be used at runtime, not compile time. This is because you cannot take the address
+// of a __thread varible in static initialization, since the address will differ per thread.
+#define UaThreadArenasInit(thread_name)                                 \
+	ua__thread_arenas_init__(                                       \
+		LM_CONCAT3(ua__thread_arenas_, thread_name, _buffer__), \
+		&LM_CONCAT3(ua__thread_arenas_, thread_name, __),       \
+		&ua__thread_arenas_instance__)
 
-void *u_arena_falloc(UArena *a, size_t size);
+#define UaThreadArenasInitExtern(thread_name)                           \
+	ua__thread_arenas_init_extern__(&LM_CONCAT3(ua__thread_arenas_, \
+						    thread_name, __),   \
+					&ua__thread_arenas_instance__)
 
-void *u_arena_fzalloc(UArena *a, size_t size);
+#define UaThreadArenasAdd(arena) \
+	ua__thread_arenas_add__(arena, ua__thread_arenas_instance__)
 
-void u_arena_release(UArena *a, size_t pos, size_t size);
+#define UaScratchGet(conflicts, conflict_count)      \
+	ua__scratch_get__(conflicts, conflict_count, \
+			  ua__thread_arenas_instance__)
 
-void u_arena_free(UArena *a);
+#define UaScratchRelease(scratch) ua__scratch_release__(scratch)
 
-void u_arena_pop(UArena *a, size_t size);
+#define UaPushArray(a, type, count) ua_alloc(a, sizeof(type) * count)
+#define UaPushArrayZero(a, type, count) ua_zalloc(a, sizeof(type) * count)
 
-void u_arena_set_alignment(UArena *a, size_t alignment);
+#define UaPushStruct(a, type) UaPushArray(a, type, 1)
+#define UaPushStructZero(a, type) UaPushArrayZero(a, type, 1)
 
-void *u_arena_pos(UArena *a);
+void ua_init(UArena *ua, bool contiguous, bool mallocd, bool bootstrapped,
+	     size_t alignment, size_t page_sz, size_t cap, uint8_t *mem);
 
-void *u_arena_seek(UArena *a, size_t pos);
+UArena *ua_create(size_t cap, bool contiguous, bool mallocd, size_t alignment);
 
-#define u_arena_array(a, type, count) u_arena_alloc(a, sizeof(type) * count)
+void ua_destroy(UArena **uap);
 
-#define u_arena_struct(a, type) u_arena_array(a, type, 1)
+UArena *ua_bootstrap(UArena *ua, UArena *new_existing, size_t cap,
+		     size_t alignment);
+
+void *ua_alloc(UArena *ua, size_t size);
+
+void *ua_zalloc(UArena *ua, size_t size);
+
+void *ua_falloc(UArena *ua, size_t size);
+
+void *ua_fzalloc(UArena *ua, size_t size);
+
+void ua_release(UArena *ua, size_t pos, size_t size);
+
+void ua_free(UArena *ua);
+
+void ua_pop(UArena *ua, size_t size);
+
+void ua_set_alignment(UArena *ua, size_t alignment);
+
+size_t ua_pos(UArena *ua);
+
+void *ua_seek(UArena *ua, size_t pos);
+
+size_t ua_reserve(UArena *ua, size_t sz);
+
+void ua__thread_arenas_init__(UArena *ta_buf[], struct ua__thread_arenas__ *tas,
+			      struct ua__thread_arenas__ **ta_instance);
+
+void ua__thread_arenas_init_extern__(struct ua__thread_arenas__ *tas,
+				     struct ua__thread_arenas__ **ta_instance);
+
+int ua__thread_arenas_add__(UArena *ua,
+			    struct ua__thread_arenas__ *ta_instance);
+
+UArenaScratch ua_scratch_begin(UArena *ua);
+
+UArenaScratch ua__scratch_get__(UArena **conflicts, int conflict_count,
+				struct ua__thread_arenas__ *tas);
+
+void ua__scratch_release__(UArenaScratch uas);
 
 #endif /* u_arena_H */
